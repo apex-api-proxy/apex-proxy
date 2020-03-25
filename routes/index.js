63905e9ebe70155ec304c7ec06ddb12df59492b5
Kelvin Wong <kjhwong@gmail.com>
2020-03-25 17:06:13 +0000

    Update formatting

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,76 @@ const options = {
   // hostname: 'dagpayapi.azurewebsites.net',
   port: 443,
   path: '/api/employee',
-  method: 'GET'
-}
+  method: 'GET',
+};
 
 /* GET home page. */
 router.get('/apex', function(req, res, next) {
-	options['hostname'] = req.headers['host'];
+  options['hostname'] = req.headers['host'];
 
-	let body = '';
+  let body = '';
 
-	const request = https.request(options, response => {
-	  console.log(`statusCode: ${res.statusCode}`)
+  console.log('options: ', options);
 
-	  response.on('data', d => {
-	    body += d;
-	  })
+  const request = https.request(options, (response) => {
+    console.log(`statusCode: ${res.statusCode}`);
 
-	  response.on('end', () => {
-	    try {
-	      const data = JSON.parse(body);
+    response.on('data', (d) => {
+      console.log('type of d: ', typeof d);
+      process.stdout.write(d);
+      body += d;
+    });
 
-	      res.send(data);
-	    } catch (er) {
-	    	console.log(er);
-	    }
-	  });
-	})
+    response.on('end', () => {
+      console.log('executing the block...');
 
-	request.on('error', error => {
-	  console.error(error)
-	})
+      try {
+        const data = JSON.parse(body);
+        // Write back something interesting to the user:
+        console.log('\ndata in the block: \n', data);
+        response.write(typeof data);
 
-	request.end();
+        res.send(data);
+      } catch (er) {}
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+  });
+
+  // request.on('end', () => {
+  // 	console.log('executing the block...');
+
+  //   try {
+  //     const data = JSON.parse(body);
+  //     // Write back something interesting to the user:
+  //     response.write(typeof data);
+  //     response.end();
+
+  //     res.send(data);
+  //   } catch (er) {
+
+  //   }
+  // });
+
+  console.log('\nafter the block\n');
+
+  request.end();
 });
 
 module.exports = router;
+
+// const req = https.request(options, res => {
+//   console.log(`statusCode: ${res.statusCode}`)
+
+//   res.on('data', d => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on('error', error => {
+//   console.error(error)
+// })
+
+// req.end()
