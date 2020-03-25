2615c1c7eb53b391cceb60135934e6e13679d754
Derick Gross <derick.gross@gmail.com>
2020-03-25 17:53:03 -0400

    add ssh to remote server

--- middleware/log.js
+++ middleware/log.js
@@ -1,3 +1,17 @@
 require('dotenv').config();
+node_ssh = require('node-ssh');
+ssh = new node_ssh();
 
 console.log(process.env.TIMESCALE_IP);
+
+ssh.connect({
+  host: `${process.env.TIMESCALE_HOSTNAME}`,
+  username: `${process.env.SSH_USERNAME}`,
+  privateKey: `${process.env.SSH_KEY_LOCATION}`
+})
+.then( _ => {
+	console.log('Connected to remote server');
+})
+.catch( e => {
+	console.log(e);
+})
