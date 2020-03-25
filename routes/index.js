fd413948e45df764439d00724fa7411cd02c7f2b
Derick Gross <derick.gross@gmail.com>
2020-03-25 10:38:05 -0400

    fetch from Dagpay

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return fetch('https://dagpayapi.azurewebsites.net/api/employee');
 });
 
 module.exports = router;
