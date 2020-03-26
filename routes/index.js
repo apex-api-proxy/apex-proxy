956f4f40d01e37dd33994987dd4d50e8326c1c50
Kelvin Wong <kjhwong@gmail.com>
2020-03-26 14:55:36 +0000

    pass through request method, path, headers, body, and response status, headers and body

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,49 @@
 const express = require('express');
 const https = require('https');
+
 const router = express.Router();
 
-const options = {
-  // hostname: 'dagpayapi.azurewebsites.net',
-  port: 443,
-  path: '/api/employee',
-  method: 'GET',
-};
+router.get('/*', (incomingRequest, outgoingResponse) => {
+  const outgoingRequestOptions = {
+    method: incomingRequest.method,
+    hostname: incomingRequest.headers['host'],
+    port: 443,
+    path: incomingRequest.path,
+    headers: incomingRequest.headers,
+  };
 
-/* GET home page. */
-router.get('/apex', function(req, res, next) {
-  options['hostname'] = req.headers['host'];
+  let incomingResponseBody = '';
+  let incomingResponseChunks = [];
 
-  let body = '';
+  const outgoingRequest = https.request(
+    outgoingRequestOptions,
+    (incomingResponse) => {
+      incomingResponse.on('data', (d) => {
+        if (incomingResponse.headers['transfer-encoding'] === 'chunked') {
+          incomingResponseChunks.push(d);
+        } else {
+          incomingResponseBody += d;
+        }
+      });
 
-  console.log('options: ', options);
+      incomingResponse.on('end', () => {
+        outgoingResponse.status(incomingResponse.statusCode);
+        outgoingResponse.set(incomingResponse.headers);
 
-  const request = https.request(options, (response) => {
-    console.log(`statusCode: ${res.statusCode}`);
+        if (incomingResponseChunks.length > 0) {
+          outgoingResponse.send(Buffer.concat(incomingResponseChunks));
+        } else {
+          outgoingResponse.send(incomingResponseBody);
+        }
+      });
+    },
+  );
 
-    response.on('data', (d) => {
-      console.log('type of d: ', typeof d);
-      process.stdout.write(d);
-      body += d;
-    });
-
-    response.on('end', () => {
-      console.log('executing the block...');
-
-      try {
-        const data = JSON.parse(body);
-        // Write back something interesting to the user:
-        console.log('\ndata in the block: \n', data);
-        response.write(typeof data);
-
-        res.send(data);
-      } catch (er) {}
-    });
-  });
-
-  request.on('error', (error) => {
+  outgoingRequest.on('error', (error) => {
     console.error(error);
   });
 
-  // request.on('end', () => {
-  // 	console.log('executing the block...');
-
-  //   try {
-  //     const data = JSON.parse(body);
-  //     // Write back something interesting to the user:
-  //     response.write(typeof data);
-  //     response.end();
-
-  //     res.send(data);
-  //   } catch (er) {
-
-  //   }
-  // });
-
-  console.log('\nafter the block\n');
-
-  request.end();
+  outgoingRequest.end();
 });
 
 module.exports = router;
-
-// const req = https.request(options, res => {
-//   console.log(`statusCode: ${res.statusCode}`)
-
-//   res.on('data', d => {
-//     process.stdout.write(d)
-//   })
-// })
-
-// req.on('error', error => {
-//   console.error(error)
-// })
-
-// req.end()
