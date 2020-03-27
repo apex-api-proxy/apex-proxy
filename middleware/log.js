a019dbc115523d1037ef74da1c4e3bcdef7083f5
Derick Gross <derick.gross@gmail.com>
2020-03-27 10:00:09 -0400

    refactor stringify; move notes to README

--- middleware/log.js
+++ middleware/log.js
@@ -51,43 +51,13 @@ const sendLog = (reqResObject) => {
 
 const reqResFormatter = (reqResObject, correlationId) => {
 	const headers = reqResObject.headers || reqResObject.getHeaders() || {};
-	const headerString = stringifyHeaders(headers);
 
 	return {
 		trace_id: correlationId,
-		headers: headerString, //? String(reqResObject.headers) : String(reqResObject.getHeaders()),
-		body: reqResObject.body ? reqResObject.body : null,
+		headers: JSON.stringify(headers), //? String(reqResObject.headers) : String(reqResObject.getHeaders()),
+		body: reqResObject.body ? JSON.stringify(reqResObject.body) : null,
 		status_code: reqResObject.statusCode ? reqResObject.statusCode : null
 	};
 };
 
-const stringifyHeaders = (headers) => {
-	return JSON.stringify(headers);
-}
-
 module.exports = apexLogger;
-
-
-/*
-TimescaleDB 'apex_log' table schema
-
-CREATE TABLE apex_log (
-	time          timestamptz NOT NULL,
-	trace_id      varchar(40) NOT NULL,
-	headers       text        NOT NULL,
-	body          text        NULL,
-	status_code   integer     NULL
-);
-ALTER TABLE apex_log ADD CONSTRAINT valid_status CHECK (status_code > 99 AND status_code < 600);
-
-Timescale insert statement, configured with object
-
-'INSERT INTO apex_log VALUES (NOW(), ${trace_id}, ${headers}, #{body}, #{status_code})', {
-	trace_id: '89678a1c-6f80-11ea-bc55-0242ac130003',
-	headers: 'test headers',
-	body: 'test body from config object'
-	status_code: 900
-}
-*/
-
-// access postgres from ubuntu prompt: sudo su - postgres
