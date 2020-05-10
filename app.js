d9f7e5a0e804481eb9e399c333b868ead95b590c
Kelvin Wong <kjhwong@gmail.com>
2020-05-10 01:58:58 +0100

    remove commented code and users route

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const { requestTracer } = require('./middleware/tracer');
 const logger = require('morgan');
-// const cookieParser = require('cookie-parser');
-// const path = require('path');
 
 const { logsDbClient, logsDbConnector } = require('./middleware/apexLogger');
 
 const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -17,12 +14,6 @@ app.use(logger('dev'));
 const client = logsDbClient();
 app.use(logsDbConnector(client));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 module.exports = app;
